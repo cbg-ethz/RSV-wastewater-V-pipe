5f068debdc70c1cc13d3067b90876a568227cd53
AugusteRi <101944341+AugusteRi@users.noreply.github.com>
2025-03-01 17:38:01 +0100

    make signatures df matrix

---
+++ utilities/shared/RSV_data_analysis/rsv_signatures.py
@@ -0,0 +1,37 @@
+import json
+import csv
+
+#Prepare binary dataframe: lineages x signature mutations
+
+#json_file = "rsv_definitions/RSVB_nucleotide_mutations_0.9.json"
+json_file = "rsv_definitions/RSVA_nucleotide_mutations_0.9.json"
+with open(json_file, "r") as file:
+    clades_definitions = json.load(file)
+
+
+
+# Collect the set of mutations
+mutations = set()
+for lineage, lineage_mutations in clades_definitions.items():
+    mutations.update(lineage_mutations)
+# Prepare the CSV data
+# First column of lineages, then mutations sorted
+header = ['Lineages'] + sorted(mutations, key=lambda x: int(''.join([char for char in x if char.isdigit()])))
+rows = []
+
+# Create rows for each lineage with 1 or 0 indicating presence of mutation
+for lineage, lineage_mutations in clades_definitions.items():
+    row = [lineage] + [1 if mutation in lineage_mutations else 0 for mutation in header[1:]]
+    rows.append(row)
+
+
+# Specify the file name
+#filename = 'rsv_signatures/rsv_b_signatures.csv'
+filename = 'rsv_signatures/rsv_a_signatures.csv'
+
+# Write to CSV
+with open(filename, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)  # Write header
+    writer.writerows(rows)   # Write data rows
+
